bb7f5df0604df6647c0973f8fb5c030476014dc4
lakhan0105 <lakhan0105@gmail.com>
2024-12-18 21:50:08 +0530

    setting up openai apo

--- src/Context/ContextProvider.jsx
+++ src/Context/ContextProvider.jsx
@@ -1,10 +1,46 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+const openaiApiKey = import.meta.env.VITE_OPENAI_API_KEY;
 
 const myContext = createContext();
 
 function ContextProvider({ children }) {
+  const [blogText, setBlogText] = useState("");
+
+  async function talkToOpenAi(prompt) {
+    const apiBody = {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Write a detailed, structured blog post based on the following input. Regardless of how vague or unclear the input is, generate a blog post that is informative, engaging, and easy to read. The blog should include an introduction, body sections with headers, and a conclusion. User's input:${prompt} .If the input is not a specific or clear topic, create a general and informative post based on common knowledge or related subjects that would still provide value to the reader.`,
+        },
+      ],
+      temperature: 0.7,
+    };
+
+    try {
+      const resp = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${openaiApiKey}`,
+        },
+        body: JSON.stringify(apiBody),
+      });
+
+      const data = await resp.json();
+      console.log(data);
+      setBlogText(data?.choices[0]?.message?.content);
+      return { blogRes: data?.choices[0]?.message?.content };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <myContext.Provider value={"something"}>{children}</myContext.Provider>
+    <myContext.Provider value={{ talkToOpenAi, blogText }}>
+      {children}
+    </myContext.Provider>
   );
 }
 
